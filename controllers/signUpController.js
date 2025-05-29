2981dd352100d8692138f99e3b1681bff2d35d81
Altay Hodoglugil <altay.hodo@gmail.com>
2025-05-28 18:40:25 -0700

    Add hashing and validating to sign up

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,12 +1,25 @@
 const db = require('../db/queries');
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 async function signUpGet(req, res) {
-  res.render('sign-up-form');
+  res.render('sign-up-form', {
+    formData: {},
+    errors: [],
+  });
 }
 
 async function signUpPost(req, res) {
+  const errors = validationResult(req);
   const { first_name, last_name, username, password } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.render('sign-up-form', {
+      errors: errors.array(),
+      formData: req.body,
+    });
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
   await db.insertUser(first_name, last_name, username, hashedPassword);
   res.redirect('/');
--- routes/signUpRouter.js
+++ routes/signUpRouter.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const signUpController = require('../controllers/signUpController')
+const signUpController = require('../controllers/signUpController');
+const { body, validationResult } = require('express-validator');
 
 router.get('/', signUpController.signUpGet);
-router.post('/', signUpController.signUpPost);
+router.post(
+  '/',
+  [
+    body('first_name')
+      .trim()
+      .notEmpty()
+      .withMessage('First name is required.')
+      .escape(),
+    body('last_name')
+      .trim()
+      .notEmpty()
+      .withMessage('Last name is required.')
+      .escape(),
+    body('username')
+      .trim()
+      .isLength({ min: 3, max: 15})
+      .withMessage('Username must be between 3 and 15 characters.'),
+    body('password')
+      .isLength({ min: 5 })
+      .withMessage('Password must be at least 5 characters long.'),
+    body('confirmPassword')
+      .custom((value, { req }) => value === req.body.password)
+      .withMessage('Passwords do not match.'),
+  ],
+  signUpController.signUpPost
+);
 
 module.exports = router;
